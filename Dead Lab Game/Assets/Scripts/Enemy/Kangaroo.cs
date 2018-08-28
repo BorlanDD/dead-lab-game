4e8129a4fa29e99bb0e6417648ec1b325bd14369
Hajruz <ruzalin4ik@mail.ru>
2018-08-28 13:49:41 +0300

    Added kangaroo animation

--- Dead Lab Game/Assets/Scripts/Enemy/Kangaroo.cs
+++ Dead Lab Game/Assets/Scripts/Enemy/Kangaroo.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class Kangaroo : Enemy {
 
+	private MeshRenderer meshRenderer;
+
+	public SkinnedMeshRenderer head;
+	public SkinnedMeshRenderer body;
+
+	public Material defaultMat;
+	public Material trancparencyMat;
+
 	// Use this for initialization
 	protected override void OnStart () {
+		base.OnStart();
 		health = 100;
+		meshRenderer = GetComponent<MeshRenderer>();
 	}
 	
 	// Update is called once per frame
 	protected override void OnUpdate () {
-		
+		base.OnUpdate();
+		//Hide
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			head.material = trancparencyMat;
+			body.material = trancparencyMat;
+		} 
+		//Unhide
+		else if (Input.GetKeyDown(KeyCode.U))
+		{
+			head.material = defaultMat;
+			body.material = defaultMat;
+		}
 	}
 
 	
